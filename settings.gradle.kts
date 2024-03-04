44e54090daf6ed92a10a573d11a0a128c6581de8
Vlad_Tonkiy <ishsdvlua839@gmail.com>
2024-03-04 12:03:34 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "competition_application"
+include(":app")
+ 
